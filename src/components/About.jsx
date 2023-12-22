37ae684ba84bc917ecd29e0265ad22d07170bd9e
Tom Kondat <tomkondat5@gmail.com>
2023-12-22 19:32:13 +0200

    Building the about section with tilt motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import { motion } from 'framer-motion'
+import { styles} from '../styles'
+import {services} from '../constants'
+import { fadeIn , textVariant } from '../utils/motion';
+
+const ServiceCard = ({index , title , icon}) => {
+  
+  return (
+    <Tilt className='xs:w-[250px] w-full' >
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div
+         options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+         }}
+         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+         flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>
+      Overview
+      </h2>
+    </motion.div>
+
+    <motion.p variants={fadeIn("","",0.1,1)}
+    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+    Computer science graduate with a strong technical foundation and excellent interpersonal skills.
+    Quick learner with a passion for technology, adaptable to evolving environments.
+    A creative and collaborative team player, eager to contribute to dynamic teams.
+    Seeking a full-time position as a full-stack developer in the tech industry.
+    </motion.p>
+
+    <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((service, index) => (
+        <ServiceCard key={service.title} index = {index} {...service}/>
+      ))}
+    </div>
+    </>
   )
 }
 
