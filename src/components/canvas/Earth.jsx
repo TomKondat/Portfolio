d95ec69af5b8e617a4a070dba2496f1376a8666b
Tom Kondat <tomkondat5@gmail.com>
2023-12-24 17:08:39 +0200

    Add stars canvas with movment

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -38,9 +38,10 @@ const EarthCanvas = () => {
         minPolarAngle={Math.PI / 2}
         />
         <Earth/>
-      </Suspense>
 
-      
+        <Preload all />
+        
+      </Suspense>
     </Canvas>
   )
 }
