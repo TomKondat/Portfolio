ef2773b2cdf4a8c6d1fb4b3ac8fbb0c7a99e06ae
Tom Kondat <tomkondat5@gmail.com>
2025-04-10 23:11:54 +0300

    switch  motion from works because of mobile bug

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,13 @@
+import React, { useEffect } from "react";
 import Tilt from "react-parallax-tilt";
-import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 
 const ProjectCard = ({
-  index,
   name,
   description,
   tags,
@@ -17,50 +16,45 @@ const ProjectCard = ({
   type,
 }) => {
   return (
-    <motion.div
-      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+    <Tilt
+      options={{ max: 45, scale: 1, speed: 450 }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       style={{ willChange: "transform" }} // Ensure smooth rendering
     >
-      <Tilt
-        options={{ max: 45, scale: 1, speed: 450 }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-        style={{ willChange: "transform" }} // Ensure smooth rendering
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
-          />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                alt="github"
-                className="w-[65%] h-[65%] object-contain"
-              />
-            </div>
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={github}
+              alt="github"
+              className="w-[65%] h-[65%] object-contain"
+            />
           </div>
         </div>
+      </div>
 
-        <div className="mt-5">
-          <h3 className="text-white text-[24px] font-bold">{name}</h3>
-          <h4 className="text-[#ff9e00] text-[16px] font-semibold">{type}</h4>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+      <div className="mt-5">
+        <h3 className="text-white text-[24px] font-bold">{name}</h3>
+        <h4 className="text-[#ff9e00] text-[16px] font-semibold">{type}</h4>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
   );
 };
 
@@ -72,26 +66,23 @@ const Works = () => {
 
   return (
     <>
-      <motion.div variants={textVariant()}>
+      {/* <div>
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects</h2>
+      </div> */}
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] mb-10">
+          Projects
+        </h2>
       </motion.div>
-
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          Here are some of the projects I have worked on
-        </motion.p>
-      </div>
 
       <div
         className="mt-20 flex flex-wrap gap-7"
         style={{ minHeight: "1px" }} // Prevent container collapse
       >
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard key={`project-${index}`} {...project} />
         ))}
       </div>
     </>
